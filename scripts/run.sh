0f811502120f9aef61f2f35e923855ecece7c4e9
Iwan Moreton <me@iwanmoreton.com>
2020-11-26 12:56:13 +0000

    add docker repo

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
-
+ssh google_iwantm_me@animalapp-manager  << EOF
+export API_CODE=iw455756477
+if [ -d "QAAnimalApp" ]; then
+    cd QAAnimalApp
+    git pull
+else
+    git clone https://github.com:iwantm/QAAnimalApp.git
+    cd QAAnimalApp/
+fi 
 docker-compose pull
 docker stack deploy --compose-file docker-compose.yaml AnimalApp
+EOF
